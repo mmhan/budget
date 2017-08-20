f6ec67d293e6bc1a7d63227843dba70c5a3d4129
mmhan <mmhan2u@gmail.com>
2017-08-20 23:21:51 +0630

    Finished up

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,51 @@
 defmodule Budget do
+
+  alias NimbleCSV.RFC4180, as: CSV
+
   def list_transactions do
-    "listing transactions"
+    File.read!("lib/transactions.csv")
+    |> parse
+    |> filter
+    |> normalize
+    |> sort
+    |> print
+  end
+
+  defp parse(string) do
+     CSV.parse_string(string)
+  end
+
+  defp filter(rows) do
+    Enum.map(rows, &Enum.drop(&1, 1))
+  end
+
+  defp normalize(rows) do
+    Enum.map(rows, &parse_amount(&1))
+  end
+
+  defp parse_amount([date, description, amount]) do
+    [date, description, parse_to_float(amount)]
+  end
+
+  defp parse_to_float(string) do
+    {float, _} = Float.parse string
+    abs float
+  end
+
+  defp sort(rows) do
+    Enum.sort(rows, &sort_asc_by_amount(&1, &2))
+  end
+
+  defp sort_asc_by_amount([_,_,previous_amount], [_,_,next_amount]) do
+    previous_amount < next_amount
+  end
+
+  defp print(rows) do
+    IO.puts "\nTransactions:"
+    Enum.each(rows, &print_to_console(&1))
+  end
+
+  defp print_to_console([date, description, amount]) do
+    IO.puts "#{date} #{description} \t\t$ #{Float.to_string(amount, decimals: 2)}"
   end
 end
